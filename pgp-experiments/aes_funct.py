f5f454a63f0b4fd33cec76f0a4842b158b21ce2a
RaspberryPi99 <camimportantemails@gmail.com>
2021-03-03 12:21:50 -0500

    Aes funct fully working

--- pgp-experiments/aes_funct.py
+++ pgp-experiments/aes_funct.py
@@ -1,6 +1,7 @@
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
+import io
 
 class AesInterface:
     """
@@ -21,20 +22,57 @@ class AesInterface:
                 print("[ ! ] Please provide proper public key")
 
         if privatefile != None:
+            #cipher_rsa = PKCS1_OAEP.new(private_key)
             self.__private_key = RSA.import_key(open(privatefile, 'r').read())
 
     def encrypt(self, data):
         if self.__public_key is not None:
-            cipher = PKCS1_OAEP.new(key=self.__public_key)
-            ciphertext = cipher.encrypt(data)
+            # Load the private key
+            try:
+                recipient_key = RSA.import_key(open("public.pem").read())
+                session_key = get_random_bytes(16)
+            except FileNotFoundError:
+                print("[!] Please Generate keys with genkey.py")
+                import sys
+                sys.exit(0)
 
-            return ciphertext
+            # Encrypt the session key with the public RSA key
+            cipher_rsa = PKCS1_OAEP.new(recipient_key)
+            enc_session_key = cipher_rsa.encrypt(session_key)
+            print(len(enc_session_key))
+            # Encrypt the data with the AES session key
+            cipher_aes = AES.new(session_key, AES.MODE_EAX)
+            ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+
+            print(len(enc_session_key + cipher_aes.nonce + tag + ciphertext))
+            return enc_session_key + cipher_aes.nonce + tag + ciphertext
+
         else:
-            print("[ ! ]Please Load Public Key")
+            print("[ ! ] Please Load Public Key")
 
     def decrypt(self, data):
-        decrypt = PKCS1_OAEP.new(key=self.__private_key)
-        data = decrypt.decrypt(data)
+        cipher_rsa = PKCS1_OAEP.new(self.__private_key)
+
+        head = 0
+
+        enc_session_key = data[head:head+self.__private_key.size_in_bytes()]
+        head += self.__private_key.size_in_bytes()
+
+        nonce = data[head:head+16]
+        head += 16
+
+        tag = data[head:head+16]
+        head += 16
+
+        ciphertext = data[head:]
+
+        print(len(enc_session_key), len(nonce), len(tag), len(ciphertext))
+        # Decrypt the session key
+        session_key = cipher_rsa.decrypt(enc_session_key)
+
+        # Decrypt the data with the AES session key
+        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
 
         return data
 
--- pgp-experiments/benchmark.py
+++ pgp-experiments/benchmark.py
@@ -1,6 +1,11 @@
 import aes_funct
 import pgp_funct
+import cv2
+from PIL import Image
+import io
+
 import random
+
 import time
 
 if __name__ in '__main__':
@@ -9,7 +14,20 @@ if __name__ in '__main__':
     pgp = pgp_funct.PgpInterface()
     pgp.load_keys(privatefile="privatekey.asc", publicfile="publickey.asc")
 
-    original = ''.join([str(random.randint(0, 9)) for i in range(random.randint(100, 254))]).encode()
+    
+    #original = ''.join([str(random.randint(0, 9)) for i in range(10000000)]).encode()
+    dimensions = [480, 640]
+    cap = cv2.VideoCapture(0) # Start camera capture session
+
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, dimensions[0]) # Set max cap height
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, dimensions[1]) # Set max cap width
+
+    ret, frame = cap.read()
+
+    img = Image.fromarray(frame, "RGB") # Convert image in numpy format to image data
+    data = io.BytesIO()
+    img.save(data, format='JPEG') # Convert image data to jpeg
+    original = data.getvalue() # get image byte data
 
     start = time.time()
     ciphertext = aes.encrypt(original)
