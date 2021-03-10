03705c9e6844c4561e878868fc84f83eeb705847
RaspberryPi99 <camimportantemails@gmail.com>
2021-03-10 11:00:20 -0500

    classes import when class is initialized

--- algorithms.py
+++ algorithms.py
@@ -22,15 +22,15 @@ class AesInterface:
         if publicfile != None:
             # Load the private key
             try:
-                self.__public_key = RSA.import_key(open(publicfile, 'r').read())
-                self.__session_key = get_random_bytes(16)
+                self.__public_key = self.RSA.import_key(open(publicfile, 'r').read())
+                self.__session_key = self.get_random_bytes(16)
             except FileNotFoundError:
                 print("[ ! ] Public Key Not Found")
 
         if privatefile != None:
             #cipher_rsa = PKCS1_OAEP.new(private_key)
             try:
-                self.__private_key = RSA.import_key(open(privatefile, 'r').read())
+                self.__private_key = self.RSA.import_key(open(privatefile, 'r').read())
             except FileNotFoundError:
                 print("[ ! ] Private Key Not Found")
 
@@ -45,11 +45,11 @@ class AesInterface:
         if self.__public_key is not None:
 
             # Encrypt the session key with the public RSA key
-            cipher_rsa = PKCS1_OAEP.new(self.__public_key)
+            cipher_rsa = self.PKCS1_OAEP.new(self.__public_key)
             enc_session_key = cipher_rsa.encrypt(self.__session_key)
 
             # Encrypt the data with the AES session key
-            cipher_aes = AES.new(self.session_key, AES.MODE_EAX)
+            cipher_aes = self.AES.new(self.__session_key, self.AES.MODE_EAX)
             ciphertext, tag = cipher_aes.encrypt_and_digest(data)
 
             return enc_session_key + cipher_aes.nonce + tag + ciphertext
@@ -60,27 +60,27 @@ class AesInterface:
 
     def decrypt(self, data):
         if self.__private_key is not None:
-            cipher_rsa = PKCS1_OAEP.new(self.__private_key)
+            cipher_rsa = self.PKCS1_OAEP.new(self.__private_key)
 
-            head = 0
+            head = 0 # Start reading from first beggining
 
-            enc_session_key = data[head:head+self.__private_key.size_in_bytes()]
+            enc_session_key = data[head:head+self.__private_key.size_in_bytes()] # Read the size of private key starting at head to get session key
             head += self.__private_key.size_in_bytes()
 
-            nonce = data[head:head+16]
+            nonce = data[head:head+16] # Read 16 bytes after to get nonce
             head += 16
 
-            tag = data[head:head+16]
+            tag = data[head:head+16] # Read 16 Bytes after to get tag
             head += 16
 
-            ciphertext = data[head:]
+            ciphertext = data[head:] # Read the rest of the file to get ciphertext
 
-            print(len(enc_session_key), len(nonce), len(tag), len(ciphertext))
             # Decrypt the session key
             session_key = cipher_rsa.decrypt(enc_session_key)
 
+
             # Decrypt the data with the AES session key
-            cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+            cipher_aes = self.AES.new(session_key, self.AES.MODE_EAX, nonce)
             data = cipher_aes.decrypt_and_verify(ciphertext, tag)
 
             return data
