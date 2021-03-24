531955bc036ccc262dfda5a62cc6cc18c33070f3
RaspberryProgramming <figueroa0609@protonmail.com>
2021-03-24 17:42:34 -0400

    Moved key generation into algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -53,6 +53,41 @@ class AesInterface:
             except FileNotFoundError:
                 print("[ ! ] Private Key Not Found")
 
+    def generateNewKeys(self, keysize=1024):
+        """
+        Generates and returns new keys
+        keysize: size of key, default at 1024
+
+        return: private_key, public_key
+        """
+        key = RSA.generate(keysize) # Generate private key
+
+        # Extract the private key
+        private_key = key.export_key()
+
+        # Extract the public key
+        public_key = key.publickey().export_key()
+        return private_key, public_key
+
+    def writeNewKeys(self, keysize=1024):
+        """
+        Write new private and public keys as private.pem and public.pem
+        keysize: size of key, default at 1024
+        """
+        key = RSA.generate(keysize) # Generate private key
+
+        # Write the private key
+        private_key = key.export_key()
+        file_out = open("private.pem", "wb")
+        file_out.write(private_key)
+        file_out.close()
+
+        # Extract and write the public key
+        public_key = key.publickey().export_key()
+        file_out = open("public.pem", "wb")
+        file_out.write(public_key)
+        file_out.close()
+
     def encrypt(self, data):
         """
         encrypt - Encrypts data using class' public key
